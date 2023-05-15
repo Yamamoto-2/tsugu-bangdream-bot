59c77e9c37c62b648ade89b45ca80602b2f9f1e7
Yamamoto-2 <1810972564qq@gmail.com>
2023-05-15 19:51:16 +0900

    fix: 下载图片持续使用缓存

--- src/api/getFile.ts
+++ src/api/getFile.ts
@@ -1,12 +1,21 @@
 import * as path from 'path';
 import { cacheRootPath } from '../config';
-import {download} from './downloader';
+import { download } from './downloader';
+import { Buffer } from 'buffer'
 
 
-async function downloadFileAndCache(url: string): Promise<unknown> {
+async function downloadFileAndCache(url: string): Promise<Buffer> {
   const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
-  const data = download(url, cacheDir, path.basename(url), true);
+  const data = download(url, cacheDir, path.basename(url), false);
+  /*
+  if (isBuffer(data)) return data;
+  else throw new Error("downloadFileAndCache: data is not a buffer")
+  */
   return data;
+}
+
+function isBuffer(variable: any): boolean {
+  return variable instanceof Buffer;
 }
 
 export { downloadFileAndCache }
