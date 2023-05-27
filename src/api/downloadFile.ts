34246d4725dc66688e69ca5c1a5887a5df49d792
Yamamoto-2 <1810972564qq@gmail.com>
2023-05-27 09:27:36 +0900

    fix: 使用临时素材，解决材质丢失导致的崩溃问题

--- src/api/downloadFile.ts
+++ src/api/downloadFile.ts
@@ -1,14 +1,22 @@
 import * as path from 'path';
-import { cacheRootPath } from '../config';
+import { cacheRootPath, assetsRootPath } from '../config';
 import { download } from './downloader';
 import { Buffer } from 'buffer'
+import * as fs from 'fs'
 
 
 async function downloadFile(url: string): Promise<Buffer> {
-  const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
+  try{  const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
   const data = await download(url, cacheDir, path.basename(url), false);
   if(data instanceof Buffer) return data as Buffer;
-  else throw new Error("downloadFile: data is not Buffer");
+  else throw new Error("downloadFile: data is not Buffer");}
+  catch(e){
+    console.log(e)
+    if(url.includes('.png')){
+      return(await fs.readFileSync(path.join(assetsRootPath,'err.png')))
+    }
+  }
+
 }
 
 export { downloadFile }
