c2d668b51c5b93cae98729230e1c147182ee4b40
Yamamoto-2 <1810972564qq@gmail.com>
2023-05-28 18:20:50 +0900

    fix: 修复自动选择服务器问题

--- src/components/list/song.ts
+++ src/components/list/song.ts
@@ -1,29 +1,29 @@
 import { Canvas, createCanvas } from "canvas"
 import { Band } from "../../types/Band"
-import { defaultserverList } from "../../types/Server"
+import { defaultserverList,getServerByPriority } from "../../types/Server"
 import { Song } from "../../types/Song"
 import { drawText, setFontStyle } from "../text"
 import { resizeImage } from "../utils"
 import { drawDifficulity } from "./difficulty"
 
 export async function drawSongInList(song: Song): Promise<Canvas> {
-    await song.initFull()
+    var server = getServerByPriority(song.publishedAt)
     var songImage = resizeImage({
         image: await song.getSongJacketImage(),
         widthMax: 80,
         heightMax: 80
     })
     var songName = drawText({
-        text: song.musicTitle[defaultserverList[0].serverId] ?? song.musicTitle[0],
+        text: server.getContentByServer(song.musicTitle),
         textSize: 26,
-        maxWidth: 500
+        maxWidth: 800
     })
     // 写法存在一些小问题，English Ver歌曲会炸
     var bandId = song.bandId
     var bandName = drawText({
-        text: new Band(bandId).bandName[defaultserverList[0].serverId],
+        text: server.getContentByServer(new Band(bandId).bandName),
         textSize: 26,
-        maxWidth: 500
+        maxWidth: 800
     })
     var tempcanv = createCanvas(800, 75)
     var ctx = tempcanv.getContext("2d")
@@ -36,7 +36,7 @@ export async function drawSongInList(song: Song): Promise<Canvas> {
         let i = parseInt(d)
         ctx.drawImage(await drawDifficulity(i, song.difficulty[i].playLevel), 800 - 55 * Object.keys(song.difficulty).length + 55 * i, 7.5, 60, 60)
     }
-    ctx.fillText(song.songId.toString(), 16, 17.5)
+    ctx.fillText(song.songId.toString(), 0, 17.5)
     ctx.drawImage(songName, 116, 0)
     ctx.drawImage(bandName, 116, 34.5)
     return tempcanv
