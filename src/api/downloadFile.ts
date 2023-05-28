b4b05e42905bdf160adf0aa7cdf58b9340988046
Yamamoto-2 <1810972564qq@gmail.com>
2023-05-28 18:43:44 +0900

    fix: 加入不用错误文件替代的下载图片方法

--- src/api/downloadFile.ts
+++ src/api/downloadFile.ts
@@ -5,15 +5,17 @@ import { Buffer } from 'buffer'
 import * as fs from 'fs'
 
 
-async function downloadFile(url: string): Promise<Buffer> {
-  try{  const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
-  const data = await download(url, cacheDir, path.basename(url), false);
-  if(data instanceof Buffer) return data as Buffer;
-  else throw new Error("downloadFile: data is not Buffer");}
-  catch(e){
+async function downloadFile(url: string, IgnoreErr: boolean = true): Promise<Buffer> {
+  try {
+    const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
+    const data = await download(url, cacheDir, path.basename(url), false);
+    if (data instanceof Buffer) return data as Buffer;
+    else throw new Error("downloadFile: data is not Buffer");
+  }
+  catch (e) {
     console.log(e)
-    if(url.includes('.png')){
-      return(await fs.readFileSync(path.join(assetsRootPath,'err.png')))
+    if (url.includes('.png') && IgnoreErr) {
+      return (await fs.readFileSync(path.join(assetsRootPath, 'err.png')))
     }
   }
 
