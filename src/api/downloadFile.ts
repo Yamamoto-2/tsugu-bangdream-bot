2b229e2d294ce29fb73370abfcaa410d00c908f4
Yamamoto-2 <1810972564qq@gmail.com>
2023-05-30 09:08:51 +0900

    fix: 修复下载器是否报错问题

--- src/api/downloadFile.ts
+++ src/api/downloadFile.ts
@@ -6,19 +6,26 @@ import * as fs from 'fs'
 
 
 async function downloadFile(url: string, IgnoreErr: boolean = true): Promise<Buffer> {
-  try {
+  if(!IgnoreErr){
     const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
     const data = await download(url, cacheDir, path.basename(url), false);
     if (data instanceof Buffer) return data as Buffer;
     else throw new Error("downloadFile: data is not Buffer");
   }
-  catch (e) {
-    console.log(e)
-    if (url.includes('.png') && IgnoreErr) {
-      return (await fs.readFileSync(path.join(assetsRootPath, 'err.png')))
+  else{
+    try {
+      const cacheDir = path.join(cacheRootPath, path.dirname(url).replace(/^[^/]+:\/\/[^/]+\//, ''));
+      const data = await download(url, cacheDir, path.basename(url), false);
+      if (data instanceof Buffer) return data as Buffer;
+      else throw new Error("downloadFile: data is not Buffer");
+    }
+    catch (e) {
+      console.log(e)
+      if (url.includes('.png')) {
+        return (await fs.readFileSync(path.join(assetsRootPath, 'err.png')))
+      }
     }
   }
-
 }
 
 export { downloadFile }
