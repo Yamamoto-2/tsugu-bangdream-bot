e83d6f5b6159a71748b5f36c8b37f48985972bcf
Yamamoto-2 <1810972564qq@gmail.com>
2023-06-09 13:22:18 +0900

    fix: 修复时间null被判断为0的错误

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,7 +1,10 @@
 //将string[]转变为number[]
-export function stringToNumberArray(stringArray: string[]): number[] {
+export function stringToNumberArray(stringArray: Array<string | null>): number[] {
     let numberArray: number[] = []
     for (let i = 0; i < stringArray.length; i++) {
+        if (stringArray[i] == null) {
+            numberArray.push(null)
+        }
         numberArray.push(Number(stringArray[i]))
     }
     return numberArray
