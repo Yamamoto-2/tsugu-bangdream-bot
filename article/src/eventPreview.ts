3f7c91160d966b363b04e9095bdf58169ada01ab
Yamamoto-2 <1810972564qq@gmail.com>
2023-12-12 17:07:23 +0900

    fix: 优化专栏生成器 eventPreview 多线程

--- article/src/eventPreview.ts
+++ article/src/eventPreview.ts
@@ -19,41 +19,43 @@ function base64ToFile(base64List: Array<{ type: 'string' | 'base64', string: str
             imageId++;
             try {
                 fs.writeFileSync(`${path}${imageId}.png`, dataBuffer);
+                console.log(`${path}${imageId}.png 保存成功！`);
             }
             catch (err) {
                 console.log(err);
             }
-            console.log(`${path}${imageId}.png 保存成功！`);
         }
+    }
+}
+
+async function processRequest(postPromise, filePath) {
+    try {
+        const response = await postPromise;
+        base64ToFile(response.data, filePath);
+    } catch (error) {
+        console.error(error);
     }
 }
 
 async function main(eventIdString: string) {
     const eventId = parseInt(eventIdString);
-    // Create an array of promises for the POST requests
-    const promises = [
-        axios.default.post(url + '/eventPreview/eventPreviewBanner', { eventId }),
-        axios.default.post(url + '/eventPreview/eventPreviewTitle', { eventId }),
-        axios.default.post(url + '/eventPreview/eventPreviewRules', { eventId }),
-        axios.default.post(url + '/eventPreview/eventPreviewCards', { eventId }),
-        axios.default.post(url + '/eventPreview/eventPreviewSongs', { eventId }),
-        axios.default.post(url + '/eventPreview/eventPreviewGacha', { eventId })
-    ];
-
-    // Use Promise.all to wait for all requests to complete
-    const responses = await Promise.all(promises);
     const projectRoot: string = path.resolve(path.dirname(__dirname));
     const outputPath = `${projectRoot}/output/eventPreview/${eventId}`
     if (!fs.existsSync(outputPath)) {
         fs.mkdirSync(outputPath, { recursive: true });
     }
-    // Process each response
-    base64ToFile(responses[0].data, `${outputPath}/0_eventPreviewBanner_`);
-    base64ToFile(responses[1].data, `${outputPath}/1_eventPreviewTitle_`);
-    base64ToFile(responses[2].data, `${outputPath}/2_eventPreviewRules_`);
-    base64ToFile(responses[3].data, `${outputPath}/3_eventPreviewCards_`);
-    base64ToFile(responses[4].data, `${outputPath}/4_eventPreviewSongs_`);
-    base64ToFile(responses[5].data, `${outputPath}/5_eventPreviewGacha_`);
+
+    const promises = [
+        processRequest(axios.default.post(url + '/eventPreview/eventPreviewBanner', { eventId }), `${outputPath}/0_eventPreviewBanner_`),
+        processRequest(axios.default.post(url + '/eventPreview/eventPreviewTitle', { eventId }), `${outputPath}/1_eventPreviewTitle_`),
+        processRequest(axios.default.post(url + '/eventPreview/eventPreviewRules', { eventId }), `${outputPath}/2_eventPreviewRules_`),
+        processRequest(axios.default.post(url + '/eventPreview/eventPreviewSongs', { eventId }), `${outputPath}/3_eventPreviewSongs_`),
+        processRequest(axios.default.post(url + '/eventPreview/eventPreviewCards', { eventId }), `${outputPath}/4_eventPreviewCards_`),
+        processRequest(axios.default.post(url + '/eventPreview/eventPreviewGacha', { eventId }), `${outputPath}/5_eventPreviewGacha_`)
+    ];
+
+    // Wait for all requests to complete
+    await Promise.allSettled(promises);
 }
 
 // Read eventId from command line
