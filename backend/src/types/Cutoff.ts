d74c88ec0467d0553361025158dbf61813d01960
Yamamoto-2 <1810972564qq@gmail.com>
2023-12-15 04:54:09 +0900

    fix: cutoff缓存判断日期增加2天，用于等待最终档线录入

--- backend/src/types/Cutoff.ts
+++ backend/src/types/Cutoff.ts
@@ -62,7 +62,7 @@ export class Cutoff {
         let cutoffData
         //如果cutoff的活动已经结束，则使用缓存
         const time = new Date().getTime()
-        if (time < this.endAt) {
+        if (time < this.endAt + 1000 * 60 * 60 * 24 * 2) {
             cutoffData = await callAPIAndCacheResponse(`${Bestdoriurl}/api/tracker/data?server=${<number>this.server}&event=${this.eventId}&tier=${this.tier}`)
         }
         else {
