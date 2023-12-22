d6aa93206cac7e388952ed789cf64f1afc372e3c
Yamamoto-2 <1810972564qq@gmail.com>
2023-12-22 09:51:32 +0900

    fix: 修复eventReport程序编译错误

--- article/pythonSrc/eventReport.py
+++ article/pythonSrc/eventReport.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 from aiohttp import ClientSession
 import shutil
+import base64
 
 # Constants
 url = 'http://tsugubot.com:8080'
@@ -15,12 +16,24 @@ async def process_request(session, endpoint, data, file_path):
     try:
         async with session.post(url + endpoint, json=data) as response:
             response_data = await response.json()
-            # Process the response data as required
-            # Example: Save response to a file
-            with open(file_path, 'w') as file:
-                file.write(str(response_data))
+            base64_to_file(response_data, file_path)
     except Exception as e:
         print(e)
+
+# Function to save base64 strings as files
+def base64_to_file(base64_list, output_path):
+    if not os.path.exists(os.path.dirname(output_path)):
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    image_id = 1
+    for item in base64_list:
+        if item['type'] == 'base64':
+            image_data = item['string']
+            with open(f"{output_path}{image_id}.png", "wb") as file:
+                file.write(base64.b64decode(image_data))
+                image_id += 1
+                print(f"{output_path}{image_id}.png saved successfully!")
+
 
 # Main function
 async def main(event_id_string):
@@ -30,12 +43,9 @@ async def main(event_id_string):
     async with ClientSession() as session:
         # Process various requests
         await process_request(session, '/eventReport/eventReportTitle', {'eventId': event_id}, f"{output_dir}/0_eventReportTitle_")
-        # ... other process_request calls ...
-
-        # Copy files
-        shutil.copyfile(f"{project_root}/assets/title1.png", f"{output_dir}/1_title1.png")
-        shutil.copyfile(f"{project_root}/assets/title2.png", f"{output_dir}/4_title2.png")
-
+        await process_request(session, '/eventReport/eventReportCutoffListOfEvent', {'eventId': event_id, 'server': 3}, f"{output_dir}/2_ycxAll_")
+        await process_request(session, '/eventReport/eventReportPlayerNumber', {'eventId': event_id, 'server': 3}, f"{output_dir}/3_eventReportPlayerNumber_")
+        await process_request(session, '/ycx', {'eventId': event_id, 'tier': 10, 'server': 3}, f"{output_dir}/5_eventReportCutoffDetailTop_")
         # Process tier list
         for tier in tier_list_of_server_cn:
             await process_request(session, '/eventReport/eventReportCutoffDetail', {'eventId': event_id, 'tier': tier, 'server': 3}, f"{output_dir}/6_eventReportCutoffDetail{tier}_")
