2150919708549a401c3ebb9e20e979a39db35a88
Yamamoto-2 <1810972564qq@gmail.com>
2024-09-06 14:11:41 +0900

    feat: 添加初次启动自动设置环境变量脚本

--- backend/run.bat
+++ backend/run.bat
@@ -1,4 +1,10 @@
 title backend
+
+IF NOT EXIST ".env" (
+    echo .env 文件不存在，复制 .env.example 到 .env
+    copy ".env.example" ".env"
+)
+
 :off
 pm2 start ecosystem.config.js
 goto off
--- backend/run_normal.bat
+++ backend/run_normal.bat
@@ -1,4 +1,10 @@
 title backend
+
+IF NOT EXIST ".env" (
+    echo .env 文件不存在，复制 .env.example 到 .env
+    copy ".env.example" ".env"
+)
+
 :off
 ts-node -r tsconfig-paths/register src/app.ts
 goto off
