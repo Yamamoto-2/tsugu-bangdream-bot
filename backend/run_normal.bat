88da8db30887279953320fde3078345558b80c64
Yamamoto-2 <1810972564qq@gmail.com>
2024-09-08 21:37:26 +0900

    fix: 回滚后端启动脚本

--- backend/run_normal.bat
+++ backend/run_normal.bat
@@ -1,10 +1,5 @@
 title backend
 
-IF NOT EXIST ".env" (
-    echo .env 文件不存在，复制 .env.example 到 .env
-    copy ".env.example" ".env"
-)
-
 :off
 ts-node -r tsconfig-paths/register src/app.ts
 goto off
